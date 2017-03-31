5acf1e7d8e6255a8b9d224d2bb309be177ad67ed
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-03-31 19:32:46 +0300

    added clojurescript skeleton

---
+++ src/eeefff_org_website/core.clj
@@ -0,0 +1,7 @@
+(ns eeefff-org-website.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/eeefff_org_website/core_test.clj
@@ -0,0 +1,7 @@
+(ns eeefff-org-website.core-test
+  (:require [clojure.test :refer :all]
+            [eeefff-org-website.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
