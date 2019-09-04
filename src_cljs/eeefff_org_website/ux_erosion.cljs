5aa0240b74302ba1461c84744327500034750fb0
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-09-04 10:43:29 +0300

    [PI][engine] add video overlays, dynamic erosion objs creation

--- src_cljs/eeefff_org_website/ux_erosion.cljs
+++ src_cljs/eeefff_org_website/ux_erosion.cljs
@@ -1,7 +1,8 @@
 (ns eeefff-org-website.ux-erosion
   (:require [oops.core :refer [oget oset! ocall oapply ocall! oapply!
                                oget+ oset!+ ocall+ oapply+ ocall!+ oapply!+]]
-            [cljs.pprint :refer [pprint]]))
+            [cljs.pprint :refer [pprint]]
+            [clojure.string :as str]))
 
 ;; jQuery.fn.random = function() {
 ;;     var randomIndex = Math.floor(Math.random() * this.length);
@@ -9,6 +10,17 @@
 ;; };
 
 
+
+
+
+(def videos
+  (str/split "01.mov.mp4 02.mp4.mp4 03.mp4.mp4 04.mp4.mp4 05.mov.mp4 06.mp4.mp4 07.mp4.mp4 08.mp4.mp4 09.mp4.mp4 10.mov.mp4 11.mov.mp4 12.mp4.mp4 13.mp4.mp4"
+             #" "))
+
+(defn mk-video-erosion-object [v]
+  (->> v
+       (assoc {:type :video}
+              :url)))
 (def phrases
   [{:type :text
     :text "Как поверить в будущее, если оно плохо отрендерено?"
@@ -18,22 +30,52 @@
    {:type :text
     :text "Как оккупировать абстракцию?"}
    {:type :text
-    :text "Поглощение на этапе концептулизации"}
-   {:type :video
-    :url "01.mp4.mp4"}
-   {:type :video
-    :url "02.mp4.mp4"}
-   {:type :video
-    :url "03.mp4.mp4"}
-   {:type :video
-    :url "04.mp4.mp4"}
-   {:type :video
-    :url "05.mp4.mp4"}])
+    :text "Поглощение на этапе концептулизации"}])
+
+(defn mk-erosion-objects []
+  (->> videos
+       (map mk-video-erosion-object)
+       (concat phrases)))
+
+(def erosion-objects
+  (mk-erosion-objects))
+
+
+;;;
+;;; APP STATE
+;;;
+(def app-state (atom {:width (.-innerWidth js/window)
+                      :height (.-innerHeight js/window)
+                      :erosion-level 0}))
+
+
+;;;
+;;; control erosion funcs
+;;;
+(defn is-enough-erosion []
+  (> (:erosion-level @app-state) 5))
+
+(defn increase-erosion-level []
+  (swap! app-state #(update % :erosion-level inc)))
+
+(defn reset-erosion-level []
+  ((swap! app-state #(assoc % :erosion-level 0))))
+
+;;;
+;;; end of control erosion funcs
+;;;
+
 
 (defn mk-replacement-video [url]
-  (str "<video class='eroded' onloadeddata=\"this.play();\" autoplay='autoplay' loop muted width=380 height=240 controls>"
-       "<source class='eroded' src='/data/parasite-interaces/" url "' type='video/mp4'>"
-       "</video>"))
+  (let [position (rand-nth ["relative" "fixed"])
+        video-width (+ 200 (rand-int 400))
+        video-height (* 8 (/ video-width 16))
+        pos-x (rand-int (:width @app-state))
+        pos-y (rand-int (:height @app-state))]
+
+    (str "<video class='eroded' onloadeddata=\"this.play();\" autoplay='autoplay' loop muted width=" video-width " height=" video-height " controls style='position:fixed;top:" pos-y "px;left:" pos-x "px;'>"
+       "<source class='eroded' src='/data/pi-02/" url "' type='video/mp4'>"
+       "</video>")))
 
 
 (defn mk-replacement-text [phrase]
@@ -87,16 +129,21 @@
                             (.. (js/jQuery e)
                                 (visible true)))))]
 
-    (pprint (str "nodes found: " (.-length nodes)))
+    (if-not (is-enough-erosion)
 
-    (.. nodes
-        random
-        (addClass "eroded")
-        (html (mk-replacement (rand-nth phrases))))))
+      (do
+        (pprint (str "nodes found: " (.-length nodes)))
+
+        (.. nodes
+            random
+            (addClass "eroded")
+            (html (mk-replacement (rand-nth erosion-objects))))
+
+        (increase-erosion-level)))))
 
 (defn start-erosion []
   (pprint "started")
-  (reset! interval (js/setInterval erode-ux 500)))
+  (reset! interval (js/setInterval erode-ux 1000)))
 
 (defn stop-screensaver-delay []
   (pprint "clearing erosion delay")
@@ -111,7 +158,8 @@
 (defn restart-screensaver []
   (stop-erosion)
   (stop-screensaver-delay)
-  (start-screensaver-delay))
+  (start-screensaver-delay)
+  (reset-erosion-level))
 
 
 (defn setup-erosion []
